76578c1570cbcc80fc5f6f42178780dc9edc817d
jane <779761563@qq.com>
2019-05-11 14:43:39 +0800

    commit

--- src/Manager.java
+++ src/Manager.java
@@ -10,22 +10,40 @@ public class Manager extends Worker {
 	}
 	//Manager类的初始化
 	public Manager(String name, int age, int salary, String department) {
-
+		super(name,age,salary,department);
+		worker=new ArrayList<>();
 	}
 
 	// 管理人员可以查询本部门员工的基本信息，跨部门查询提示权限不足，提示“Access Denied!”
 	public String inquire(Worker e) {
-		return null;
+		if(e.getDepartment().equals(this.department)){
+			return e.show();
+		}else{
+			return "Access Denied!";
+		}
 	}
 
 	// 管理人员给自己的队伍添加工作人员，同一部门的工作人员可以添加，并返回true，不同部门的工作人员无法添加，返回false
 	public boolean lead(Worker e) {
-		return false;
+		if(e.getDepartment().equals(this.department)){
+			worker.add(e);
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 	// 打印自己队伍的人员姓名，没有打印“Empty”
 	public String print() {
-		return null;
+		if(worker.size()==0){
+			return "Empty";
+		}else{
+			String res="";
+			for(int i=0;i<worker.size();i++){
+				res=res+" "+worker.get(i).getName();
+			}
+			return res;
+		}
 	}
 
 }
